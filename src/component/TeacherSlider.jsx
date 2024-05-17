e19103cc211357b2f84d05af38af2dfc353b12e0
Babariya Prince <princebabariya097@gmail.com>
2024-05-17 10:10:56 +0530

    add search functionality in teachers page

--- src/component/TeacherSlider.jsx
+++ src/component/TeacherSlider.jsx
@@ -1,33 +1,49 @@
-import React, { useRef } from 'react'
-import TeachersData from './Datas/TeachersData.js'
+import React, { useRef, useEffect, useState } from 'react'
+// import TeachersData from './Datas/TeachersData.js'
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLessThan, faGreaterThan } from '@fortawesome/free-solid-svg-icons';
 
-function TeacherSlider() {
+function TeacherSlider({searchName, setSearchName}) {
 
     const scrollRef = useRef(null)
+    const [teachersData, setTeachersData] = useState([])
 
     const handleScroll = (scrollOffset) => {
         scrollRef.current.scrollLeft += scrollOffset;
       };
+      
+    const apiData = async () => {
+        const response = await fetch('https://dummyjson.com/users')
+        const data = await response.json()
+        return data.users
+    }
 
-
+    useEffect(() => {
+        if (searchName.length > 0) {
+            const filteredData = teachersData.filter((teacher) => teacher.firstName.toLowerCase().includes(searchName.toLowerCase()))
+            setTeachersData(filteredData)
+            console.log(searchName);
+            } else {
+                apiData().then((data) => setTeachersData(data))
+                }
+    },[setSearchName, searchName])
+    
     return (
         <>
             <div className='flex flex-col flex-wrap h-[1365px] overflow-scroll ' ref={scrollRef}>
                 {
-                    TeachersData.map((data, index) => (
+                    teachersData.map((data) => (
                         <div key={data.id * 999} className=' h-[415px] w-[300px] shadow-lg lg:m-5'>
                             <div className=' w-[144px] h-[177px] my-7 flex m-auto'>
-                                <img src={data.img} alt="Teachers Photo" />
+                                <img src={data.image} alt="Teachers Photo" />
                             </div>
                             <div className='pl-2'>
-                                <p className='font-bold text-sm whitespace-break-spaces mb-1'>Name       : <span className='font-normal text-base'>{data.name}</span></p>
-                                <p className='font-bold text-sm whitespace-break-spaces mb-1'>Nip           : <span className='font-normal text-base'>{data.Nip}</span></p>
-                                <p className='font-bold text-sm whitespace-break-spaces'>Pelajaran : <span className='font-normal text-base'>{data.Pelajaran}</span></p>
+                                <p className='font-bold text-sm whitespace-break-spaces mb-1'>Name       : <span className='font-normal text-base'>{data.firstName}</span></p>
+                                <p className='font-bold text-sm whitespace-break-spaces mb-1'>Nip           : <span className='font-normal text-base'>{data.ssn}</span></p>
+                                <p className='font-bold text-sm whitespace-break-spaces'>Email        : <span className='font-normal text-base'>{data.email}</span></p>
                             </div>
                         </div>
                     ))
--- src/pages/Teachers.jsx
+++ src/pages/Teachers.jsx
@@ -1,31 +1,44 @@
-import React from 'react'
-import CustomHeader from '../component/CustomHeader'
-import Footer from '../component/Footer'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
-import TeacherSlider from '../component/TeacherSlider'
+import React, { useState } from "react";
+import CustomHeader from "../component/CustomHeader";
+import Footer from "../component/Footer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import TeacherSlider from "../component/TeacherSlider";
 
 function Teachers() {
+  const [searchName, setSearchName] = useState("");
+
   return (
     <>
-    <CustomHeader title={'Our Best Feaculty'} subTitle={'Data Guru - Guru SMP Negeri 1 Cibadak'} />
-    <div className=' w-[70%] mx-auto mt-7 '>
-        <div className=' h-[48px] lg:w-[400px] bg-gray-100 flex items-center  float-right'>
-            <div className='ml-1'>
+      <CustomHeader
+        title={"Our Best Feaculty"}
+        subTitle={"Data Guru - Guru SMP Negeri 1 Cibadak"}
+      />
+      <div className=" w-[70%] mx-auto mt-7 ">
+        <div className=" h-[48px] lg:w-[400px] bg-gray-100 flex items-center  float-right">
+          <div className="ml-1">
             <label htmlFor="search">
-                <FontAwesomeIcon icon={faSearch} size='sm'/>
+              <FontAwesomeIcon icon={faSearch} size="sm" />
             </label>
-            </div>  
-            <div className='ml-3 h-full w-full'>
-                <input id='search' type="text" className='bg-transparent h-full w-full focus:outline-none' placeholder='Search Teacher' />
-            </div>
+          </div>
+          <div className="ml-3 h-full w-full">
+            <input
+              id="search"
+              type="text"
+              className="bg-transparent h-full w-full focus:outline-none"
+              placeholder="Search Teacher Name"
+              onChange={(e) => setSearchName(e.target.value)}
+            />
+          </div>
         </div>
-    </div>
-    <div className=' w-[70%] h-[2px] bg-[#DFDFDF] mx-auto mb-20 mt-28 '></div>
-    <div className=' lg:w-[65.3%] md:w-[75%] max-[426px]:w-[70%] mx-auto '><TeacherSlider/></div>
-    <Footer/>
+      </div>
+      <div className=" w-[70%] h-[2px] bg-[#DFDFDF] mx-auto mb-20 mt-28 "></div>
+      <div className=" lg:w-[65.3%] md:w-[75%] max-[426px]:w-[70%] mx-auto ">
+        <TeacherSlider searchName={searchName} setSearchName={setSearchName} />
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default Teachers
+export default Teachers;
